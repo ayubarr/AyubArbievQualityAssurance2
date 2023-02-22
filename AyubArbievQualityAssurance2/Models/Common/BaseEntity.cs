f2f16f20e9fd6ba4ce2388db1286ba8321f3ab30
ayubarr <114123666+ayubarr@users.noreply.github.com>
2023-02-22 16:24:01 +0600

    Model done

---
+++ AyubArbievQualityAssurance2/Models/Common/BaseEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AyubArbievQualityAssurance2.Data.Models.Common
+{
+    internal class BaseEntity
+    {
+    }
+}
